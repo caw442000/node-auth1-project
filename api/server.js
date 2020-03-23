db3c91a44a421fef22cdee96980da0d8070f8371
cedric-winbush <cawinbushjr@gmail.com>
2020-03-23 14:07:38 -0500

    config dbConfig and knex

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const cors = require('cors');
 
+const usersRouter = require('../users/users-router.js')
+const authRouter = require('../auth/router.js')
 const server = express();
 
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(cors());
+
+
+server.get('/', (req, res) => {
+  res.status(200).json({api: "up"});
+})
 module.exports = server;
---
+++ auth/router.js
@@ -0,0 +1,5 @@
+const bcrypt = require('bcryptjs');
+
+const router = require('express').Router();
+
+module.exports = router;
