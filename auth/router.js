fc41bd6dc8948483034cb2810947e18eaf889c00
cedric-winbush <cawinbushjr@gmail.com>
2020-03-23 15:09:07 -0500

    build user model

--- auth/router.js
+++ auth/router.js
@@ -2,4 +2,12 @@ const bcrypt = require('bcryptjs');
 
 const router = require('express').Router();
 
+const Users = require('../users/users-model.js');
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  
+})
+
 module.exports = router;
